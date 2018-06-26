b079c87c5ff47fc5cf2cc45b25343cc314de1f03
huangqizai <17710946884@163.com>
2018-06-26 15:07:21 +0800

    提交webSocket

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WebSocket' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for WebSocket
+
+  target 'WebSocketTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WebSocketUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+pod 'SocketRocket', '~> 0.5.1'
+end
--- WebSocket/ViewController.m
+++ WebSocket/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "SocketRocketManager.h"
 @interface ViewController ()
 
 @end
@@ -16,7 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+   
+    [[SocketRocketManager instance] SRWebSocketOpenWithURLString:@"http://172.24.120.26:8510/sync"];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(SRWebSocketDidOpen) name:kWebSocketDidOpenNote object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(SRWebSocketDidReceiveMsg:) name:kWebSocketdidReceiveMessageNote object:nil];
+}
+
+- (void)SRWebSocketDidOpen {
+    NSLog(@"开启成功");
+    //在成功后需要做的操作。。。
+    
+}
+
+- (void)SRWebSocketDidReceiveMsg:(NSNotification *)note {
+    //收到服务端发送过来的消息
+    NSString * message = note.object;
+    NSLog(@"%@",message);
 }
 
 
