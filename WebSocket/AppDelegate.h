9c48704ff6a10d24a1d9e6ea6029cd238ec2d4bf
huangqizai <17710946884@163.com>
2018-06-26 14:36:04 +0800

    Initial Commit

---
+++ WebSocket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebSocket
+//
+//  Created by huangqizai on 2018/6/26.
+//  Copyright © 2018年 FYCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
